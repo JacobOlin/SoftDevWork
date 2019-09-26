34d281567f4f02bf7bad01ff4f0a81a59d275e28
Jacob Olin <jolin00@stuy.edu>
2019-09-26 00:14:08 -0400

    The website now redirects after submitting your input

--- test/app.py
+++ test/app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request
-import random
 app = Flask(__name__)
 
 
 @app.route("/")
 def occupation():
-    print(app)
-    print(request)
-    print(request.args)
+    return render_template('test.html')
 
-    return render_template("/test.html")
+@app.route("/auth")
+def stuff():
+    print(request.form)
+    print(app)
+    return request.args["username"]
+
 
 if __name__ == "__main__":
     app.debug = True
