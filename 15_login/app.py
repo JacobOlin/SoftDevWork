71f879aedf3f036fdf907706172ea8bda0c4604e
Jacob Olin <jolin00@stuy.edu>
2019-10-03 09:29:40 -0400

    Made some progress on the assignment

--- 15_login/app.py
+++ 15_login/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, session
 app = Flask(__name__)
 
+users = {'adminUser' : 'password'}
+
 
 @app.route("/")
 def input():
@@ -10,10 +12,14 @@ def input():
 @app.route("/login")
 def login():
     print(request.args)
-    secret_key = "hi"
-    session["hi"] = request.args["username"]
+    #secret_key = "hi"
+    #session["hi"] = request.args["username"]
     #print (request.cookies.get(user))
-    return render_template('welcome.html')
+    if users[request.args["Username"]] == request.args["Password"]:
+        return render_template('welcome.html',
+                                user = request.args["Username"] )
+    else :
+        return render_template('invalid.html')
 
 
 
