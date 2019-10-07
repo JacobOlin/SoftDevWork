1a56e52e842a5099914035d56bceb6ccf3cb9716
Jacob Olin <jolin00@stuy.edu>
2019-10-07 09:31:27 -0400

    Added in the baseline code

---
+++ 17_csv2db/app.py
@@ -0,0 +1,29 @@
+#Jacob Olin
+#SoftDev
+#
+#Oct 2019
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitate CSV I/O
+
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+
+#==========================================================
+
+# < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
+
+file = open("courses.csv",'r')
+for line in file:
+
+
+command = ""          # test SQL stmt in sqlite3 shell, save as string
+c.execute(command)    # run SQL statement
+
+#==========================================================
+
+db.commit() #save changes
+db.close()  #close database
