13a03fb585e284c1555c6c796e8e9ea339912b4e
Jacob Olin <jolin00@stuy.edu>
2019-10-08 09:22:26 -0400

    Tested some of the commands in the terminal

--- 17_csv2db/app.py
+++ 17_csv2db/app.py
@@ -15,9 +15,26 @@ c = db.cursor()               #facilitate db ops
 #==========================================================
 
 # < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
-
-file = open("courses.csv",'r')
+doneHeader = False
+file = open("static/courses.csv",'r')
 for line in file:
+    i = 0
+    if line[i] != ',':
+        i += 1
+    word1 = line[0:i]
+    i += 1
+    last = i
+    if line[i] != ',':
+        i += 1
+    word2 = line[last:i]
+    word3 = line[i+1:len(line)]
+    if (!doneHeader):
+        command = "CREATE TABLE courses (word1 TEXT, word2 INTEGER, word3 INTEGER)"
+        c.execute(command)
+    else:
+        command = "INSERT INTO courses VALUES (word1 TEXT, word2 INTEGER, word3 INTEGER)"
+        c.execute(command)
+
 
 
 command = ""          # test SQL stmt in sqlite3 shell, save as string
