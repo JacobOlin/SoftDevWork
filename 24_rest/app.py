652dab9af83f74b36e42fa5fe3f6fa15a5430722
Jacob Olin <jolin00@stuy.edu>
2019-11-13 00:58:52 -0500

    finished the assignment

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template
-import urllib3, json
+from urllib.request import urlopen
+import json
 app = Flask(__name__) #create instance of class Flask
 
 @app.route("/")
 def root():
-    u = urllib3.urlopen(
+    u = urlopen(
     "https://api.nasa.gov/planetary/apod?api_key=Ed3mcpv2KwzF7cfx3ShUHdhsv49LJJQUGHCZzKGy")
 
     response = u.read()
     data = json.loads(response)
     return render_template("index.html",
-                            pic = data['url'])
+                            pic = data['url'],
+                            expln = data['explanation'])
 
 if __name__ == "__main__":
     app.debug = True
