c957f17eb78b8480a8c0fbf05d3983c6e9cdc56b
JacobOlin <43055609+JacobOlin@users.noreply.github.com>
2019-11-14 07:54:23 -0500

    Added partner's code

--- 25_restrio_pluggedin/app.py
+++ 25_restrio_pluggedin/app.py
@@ -1,12 +1,37 @@
-from flask import Flask, render_template
-#from urllib.request import Request, urlopen
-import random, urllib, json
-app = Flask(__name__) #create instance of class Flask
+#Peihua Huang, Jacob Olin
+#SoftDev1 pd2
+#K25 -- Getting More REST
+#2019-11-12
+
+import urllib, json, random
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
 
 @app.route("/")
-def hi():
-    return "hi"
+def root():
+    return render_template("index.html")
 
+@app.route("/ghibli")
+def ghibli():
+    u = urllib.request.urlopen("https://ghibliapi.herokuapp.com/locations")
+    response = u.read()
+    data = json.loads(response)
+    return render_template("ghibli.html", locations=data)
+
+@app.route("/ddclasses")
+def cards():
+    u = urllib.request.urlopen("http://www.dnd5eapi.co/api/classes")
+    response = u.read()
+    data = json.loads(response)
+    return render_template("results.html", classes=data["results"])
+
+@app.route("/weather")
+def loripsum():
+    u = urllib.request.urlopen("https://api.darksky.net/forecast/14bbf557960676ac31fd1ee5266f5da7/40.718100,-74.014760")
+    response = u.read()
+    data = json.loads(response)
+    return render_template("weather.html", info=data["currently"])
 
 @app.route("/nhl")
 def nhl():
@@ -65,7 +90,6 @@ def super():
                             publish = data['biography']['publisher'],
                             connecs = data['connections']['group-affiliation']
                             )
-
 
 
 if __name__ == "__main__":
