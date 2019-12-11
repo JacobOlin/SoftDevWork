29a83dbaa926ed584fe9b8a1a15a76200af75e29
Jacob Olin <jolin00@stuy.edu>
2019-12-11 09:29:03 -0500

    Starting working on 28_jsdom

---
+++ 28_jsdom/funky_town.js
@@ -0,0 +1,36 @@
+var fact = function(n){
+  if (n == 1){
+    return 1;
+  }
+  return n * fact(n - 1);
+}
+
+var fib = function(n){
+    if (n > 1){
+	return fib(n-1) + fib(n-2);
+    }
+    if (n == 1){
+	return 1;
+    }
+    return 0;
+}
+
+var gcd = function(a,b){
+    while( b ){
+	var x = b;
+	b = a % x;
+	a = x;
+    }
+    return a;
+}
+
+var studentlist = ["Bob", "Jane", "Ed", "Chan", "Jason", "Hoiley", "TP", "Steve", "Vivi"];
+
+var randomStudent = function(a){
+    var l = a.length;
+    var rand = Math.floor(Math.random(l) * l + 1);
+    return a[rand - 1];
+}
+
+var fact0 = document.getElementById("fact")
+fact0.addEventListener('click',function(){fact(10)})
