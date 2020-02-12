eefc448a3c80d472e9fe4d78f18b3ccd84d52c5c
Jacob Olin <jolin00@stuy.edu>
2020-02-12 18:16:37 -0500

    Finished assignment

--- 07_canvas-anim/anim.js
+++ 07_canvas-anim/anim.js
@@ -1,31 +1,30 @@
 //Jacob Olin
 //SoftDev1 pd2
-//KK #06: Dot Dot Dot
-//2020-02-12
+//K #07: They lock us in the tower whenever we get caught ...which is often
+//2020-02-13
 
 var c = document.getElementById("start");
 var d = document.getElementById("stop");
 var can = document.getElementById("slate");
 var ctx = can.getContext("2d");
 var radius = 0;
-var stopped = false;
+var running = false;
 var inc = 1;
 var id;
 
 
 var anim = function(e) {
-  w = 500
-  h = 500
+  w = 500;
+  h = 500;
   ctx.clearRect(0,0,w,h);
   ctx.beginPath();
 
   ctx.arc(w/2, h/2, radius, 0, 2 * Math.PI);
+  ctx.fillStyle = "#1FBED6";
   ctx.fill();
-
   radius += 1*inc
   if ((radius < 250 && inc == 1) || (radius > 0 && inc == -1)){
-    //id = window.requestAnimationFrame(anim);
-    window.requestAnimationFrame(anim);
+    id = window.requestAnimationFrame(anim);
   }
   else{
     inc *= -1;
@@ -33,10 +32,18 @@ var anim = function(e) {
   }
 
 }
-c.addEventListener("click", anim);
+
+var start_anim = function(e){
+    if (!running){
+      running = true;
+      id = window.requestAnimationFrame(anim);
+    }
+}
+c.addEventListener("click", start_anim);
 
 
 var stop = function(e) {
     window.cancelAnimationFrame(id);
+    running = false;
 }
-c.addEventListener("click", stop);
+d.addEventListener("click", stop);
