7d627072d898fee4ebb0c8acfafc34dcb8cafee7
Jacob Olin <jolin00@stuy.edu>
2020-02-13 09:36:14 -0500

    Got an object to bounce off of walls

--- 08_canvas-anim/anim.js
+++ 08_canvas-anim/anim.js
@@ -5,12 +5,16 @@
 
 var c = document.getElementById("circle");
 var end = document.getElementById("stop");
+var dvd = document.getElementById("dvd");
 var can = document.getElementById("slate");
 var ctx = can.getContext("2d");
 var radius = 0;
 var running = false;
 var inc = 1;
 var id;
+var logo = new Image();
+var x,y,vx,vy;
+logo.src = "logo_dvd.jpg"
 
 
 var anim = function(e) {
@@ -37,6 +41,40 @@ var start_anim = function(e){
 }
 c.addEventListener("click", start_anim);
 
+var dvd_anim = function(e) {
+  ctx.clearRect(0,0,500,500);
+  ctx.beginPath();
+  ctx.arc(x,y,2,0,2*Math.PI);
+  ctx.fill();
+  if (x <= 0 || x >= 500){
+    vx *= -1;
+    console.log('hi');
+  }
+  if (y <= 0 || y >= 500){
+    vy *= -1;
+    console.log("hi");
+  }
+  x += vx;
+  y += vy;
+  id = window.requestAnimationFrame(dvd_anim);
+}
+
+var dvd_anim_start = function(e){
+  x = Math.random() * 500;
+  y = Math.random() * 500;
+  if (Math.random() < 0.5) {
+    vx = 1;
+  }
+  else {
+    vx = -1;
+  }
+  if (Math.random() < 0.5){
+    vy = 1;
+  }
+  else {vy = -1;}
+  id = window.requestAnimationFrame(dvd_anim);
+}
+dvd.addEventListener("click",dvd_anim_start);
 
 var stop = function(e) {
     window.cancelAnimationFrame(id);  //stops the animation
