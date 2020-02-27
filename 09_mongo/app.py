6d956e45f04376a482b4c8a9b0436b690870ebc5
Jacob Olin <jolin00@stuy.edu>
2020-02-27 14:23:38 +0000

    Asdf

--- 09_mongo/app.py
+++ 09_mongo/app.py
@@ -1,5 +1,3 @@
 from pymongo import MongoClient
-from pprint import pprint
-client = MongoClient("mongodb://127.0.0.1:27017")
-db=client.admin
-serverStatusResult=db.command("serverStatus")
+client = MongoClient('localhost',27017)
+db=client.restaurant
