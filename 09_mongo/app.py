1781683673a97925f40788bc638b52ee0c6b1621
Jacob Olin <jolin00@stuy.edu>
2020-02-28 00:06:53 -0500

    Ad

--- 09_mongo/app.py
+++ 09_mongo/app.py
@@ -1,3 +1,9 @@
+import pymongo
 from pymongo import MongoClient
+
 client = MongoClient('localhost',27017)
 db=client.restaurant
+
+file = open("primer-dataset.json", 'r')
+data = file.readlines()
+print(data)
