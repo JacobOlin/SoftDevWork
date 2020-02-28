32afb7f2588b6b2aedd206064940b90adcacb848
Jacob Olin <jolin00@stuy.edu>
2020-02-28 05:17:10 +0000

    database loads

--- 09_mongo/app.py
+++ 09_mongo/app.py
@@ -1,15 +1,17 @@
 import pymongo
 from pymongo import MongoClient
+from bson.json_util import loads
 
 client = MongoClient('localhost',27017)
-db=client.restaurant
+db=client.buildings
 restaurant = db.restaurant
 
 file = open("primer-dataset.json", 'r')
 data = file.readlines()
+data = list(map(lambda doc: loads(doc.strip()), data))
 file.close()
 #print(data)
 
-restaurant.inster_many(data)
+restaurant.insert_many(data)
 
-print (restaurant.find({"address.building" : "764"}))
+print (restaurant.find({"address.building" : "425"}))
