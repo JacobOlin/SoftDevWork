830b359ca5941bc47af34bdf29a357858ac1faa0
Jacob Olin <jolin00@stuy.edu>
2020-02-28 00:11:48 -0500

    Added data to mongodb

--- 09_mongo/app.py
+++ 09_mongo/app.py
@@ -3,7 +3,13 @@ from pymongo import MongoClient
 
 client = MongoClient('localhost',27017)
 db=client.restaurant
+restaurant = db.restaurant
 
 file = open("primer-dataset.json", 'r')
 data = file.readlines()
-print(data)
+file.close()
+#print(data)
+
+restaurant.inster_many(documents)
+
+print (restaurant.find({"address.building" : "764"}))
